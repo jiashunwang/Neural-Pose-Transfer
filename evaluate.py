b3a85387035b0dc97b6c7c6c43b8c4fbecf3dd9a
Jiashun Wang <51504170+jiashunwang@users.noreply.github.com>
2020-10-20 18:34:55 +0800

    evaluate
    code for evaluate

---
+++ evaluate.py
@@ -0,0 +1,21 @@
+import trimesh
+import numpy as np
+
+#make sure the order of identity points and gt points are same 
+#for original_model, please keep the identity and pose points in different order
+
+ours_mesh = trimesh.load('ours.obj')
+ours_vertices=ours_mesh.vertices
+ours_bbox= np.array([[np.max(ours_vertices[:,0]), np.max(ours_vertices[:,1]), np.max(ours_vertices[:,2])], \
+                        [np.min(ours_vertices[:,0]), np.min(ours_vertices[:,1]), np.min(ours_vertices[:,2])]])
+
+ours_vertices_align=ours_vertices-(ours_bbox[0] + ours_bbox[1]) / 2
+
+gt_mesh=trimesh.load('gt.obj')
+gt_vertices=gt_mesh.vertices
+gt_bbox= np.array([[np.max(gt_vertices[:,0]), np.max(gt_vertices[:,1]), np.max(gt_vertices[:,2])], \
+                        [np.min(gt_vertices[:,0]), np.min(gt_vertices[:,1]), np.min(gt_vertices[:,2])]])
+gt_vertices_align=gt_vertices-(ours_bbox[0] + ours_bbox[1]) / 2
+
+print(np.mean((ours_vertices_align-gt_vertices_align)**2))
+
